67873127ac7df7e7ff9f0fdb4b31e7c4c0b3277d
Dawid Pieciul <pieciul.dawid@gmail.com>
2021-11-14 14:00:29 +0100

    Doadanie usera

---
+++ Sklep_internetowy_DD_DP/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sklep_internetowy_DD_DP.Models;
+
+namespace Sklep_internetowy_DD_DP.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = user.Name + ", " + user.Surname + ", " + user.Email + ", " + user.Password;
+            }
+            else
+            {
+                ViewBag.Message = "Błędne dane!";
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ Sklep_internetowy_DD_DP/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sklep_internetowy_DD_DP.Models
+{
+    public class User
+    {
+        [Display(Name = "Imię")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
